96e9efb66472f168c9a709649e4d17f7f5ee7b11
aniketgope00 <aniketgope00@gmail.com>
2024-11-25 15:39:58 +0530

    getting csv code in temp

---
+++ pull_data_to_df.py
@@ -0,0 +1,10 @@
+import os
+import supabase
+import pandas as pd
+import batch_processor
+
+def create_buckets()
+
+
+df = pd.DataFrame(columns=["rms_level , spectral_centroid , bandwidth , zero_crossing_rate , band_energy_ratio , delta_spectrum_magnitude , pitch , pitch_strength , mfcc_mean , mfcc_std , average_roughness , std_roughness , one_2hz_loudness , three_15hz_loudness , twenty_43hz_loudness , sharpness , gamma_0Hz_energy , gamma_3_15Hz_energy , gamma_20_150Hz_energy , gamma_150_1000Hz_energy"])
+
